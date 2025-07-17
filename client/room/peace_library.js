271a08a4f1647d3abc257b50f41d85eeb60a87d0
Nuit62 <nurtdinovartem917@gmail.com>
2025-07-17 13:21:15 +0400

    Update peace_library.js

--- client/room/peace_library.js
+++ client/room/peace_library.js
@@ -10,7 +10,16 @@ Teams.OnRequestJoinTeam.Add((player, team) => {
 Damage.FriendlyFire = true;
 BreackGraph.OnlyPlayerBlocksDmg = false;
 BreackGraph.WeakBlocks = false;
-BreackGraph.BreackAll = true;
+BreackGraph.BreackAll = false;
 
- Teams.Get("Red").Spawns.SpawnPointsGroups.Add(2); room.Teams.OnRequestJoinTeam.Add(function (player, team) { team.Add(player); }); room.Teams.OnPlayerChangeTeam.Add(function (player) { player.Spawns.Spawn(); });
+export const BLUE_TEAM_NAME = "Blue";
+export const BLUE_TEAM_DISPLAY_NAME = "Teams/Blue";
+export const BLUE_TEAM_SPAWN_POINTS_GROUP = 1
+
+export function create_team_blue() {
+    Teams.Add(BLUE_TEAM_NAME, BLUE_TEAM_DISPLAY_NAME, new Color(0, 0, 1, 0));
+    Teams.Get(BLUE_TEAM_NAME).Spawns.SpawnPointsGroups.Add(BLUE_TEAM_SPAWN_POINTS_GROUP);
+    return Teams.Get(BLUE_TEAM_NAME);
+}
+ room.Teams.OnRequestJoinTeam.Add(function (player, team) { team.Add(player); }); room.Teams.OnPlayerChangeTeam.Add(function (player) { player.Spawns.Spawn(); });
 }
