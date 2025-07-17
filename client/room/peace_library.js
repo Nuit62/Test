e9be05eb1b385b68cb577c5aab82dbfff6b57417
Nuit62 <nurtdinovartem917@gmail.com>
2025-07-17 13:17:29 +0400

    Update peace_library.js

--- client/room/peace_library.js
+++ client/room/peace_library.js
@@ -1,8 +1,10 @@
 import * as room from 'pixel_combats/room';
 import * as teams from './default_teams.js';
 
-    // по запросу на вход в команду - кидаем игрока в команду
-    room.Teams.OnRequestJoinTeam.Add(function (player, team) { team.Add(player); });
-    // если игрок сменил команду или выбрал ее, то происходит спавн игрока
-    room.Teams.OnPlayerChangeTeam.Add(function (player) { player.Spawns.Spawn(); });
+player.Properties.Get("Number").Value = player.IdInRoom; 
+Teams.OnRequestJoinTeam.Add((player, team) => {
+    player.Properties.Get("Number").Value = player.IdInRoom;
+    team.Add(player);
+});
+ room.Teams.OnRequestJoinTeam.Add(function (player, team) { team.Add(player); }); room.Teams.OnPlayerChangeTeam.Add(function (player) { player.Spawns.Spawn(); });
 }
